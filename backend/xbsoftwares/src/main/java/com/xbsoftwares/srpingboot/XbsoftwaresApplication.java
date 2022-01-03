902854260049676310d907c4d65c29e485fbb70f
leopassosleite <leopassos.leite4@gmail.com>
2022-01-03 18:19:40 -0300

    Project created & configure MySQL

---
+++ backend/xbsoftwares/src/main/java/com/xbsoftwares/srpingboot/XbsoftwaresApplication.java
@@ -0,0 +1,13 @@
+package com.xbsoftwares.srpingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbsoftwaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbsoftwaresApplication.class, args);
+	}
+
+}
---
+++ backend/xbsoftwares/src/test/java/com/xbsoftwares/srpingboot/XbsoftwaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbsoftwares.srpingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbsoftwaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
